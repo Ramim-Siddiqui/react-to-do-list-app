0dd43879b075005351984ecd24b26142efe1d1ac
Ramim Siddiqui <ramimalisiddiqui0@gmail.com>
2023-11-14 20:25:28 +0500

    all done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Herosection from './components/Herosection';
-//import TodoApp from './components/TodoApp';
 function App() {
   return (
     <div>
--- src/components/Herosection.js
+++ src/components/Herosection.js
@@ -1,20 +1,52 @@
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
 export default function Herosection()
 {
+    const [task, setTask] = useState('');
+    const [tasks, setTasks] = useState([]);
+
+    const addTask = () => {
+        if (task.trim() !== '') {
+            setTasks([...tasks, task]);
+            setTask('');
+        }
+    };
+
+    const removeTask = (index) => {
+        const updatedTasks = [...tasks];
+        updatedTasks.splice(index, 1);
+        setTasks(updatedTasks);
+    };
     return(
         <div className="container">
             <div className="row justify-content-center text-right" style={textbox}>
                 <div className="col-md-12 col-sm-12">
-                <span>Enter the task to do</span>
-                <Form.Control type="text" placeholder="Normal text" />
-                <br/>
-                <Button variant="success">Add Task</Button>{' '}
+                <Form>
+                     <Form.Group controlId="taskInput">
+                        <span>Enter The Task To Do</span>
+                        <Form.Control type="text" placeholder="Enter text" value={task} onChange={(e) => setTask(e.target.value)}/>
+                    </Form.Group>
+                     <br/>
+                    <Button variant="success"onClick={addTask}>Add Task</Button>{' '}
+                </Form>
                 </div>
             </div>
-            <div className="row justify-content-center text-center">
+            <div className="row">
                 <div className="col-md-12 col-sm-12">
-                
+                    <ListGroup>
+                        {tasks.map((t, index) => (
+                            <ListGroup.Item key={index}>
+                                <div className="d-flex justify-content-between align-items-center">
+                                    <span>{t}</span>
+                                    <Button variant="danger" size="md" onClick={() => removeTask(index)}>
+                                        Remove
+                                    </Button>
+                                </div>
+                            </ListGroup.Item>
+                        ))}
+                    </ListGroup>
                 </div>
             </div>
         </div>
@@ -23,5 +55,7 @@ export default function Herosection()
 const textbox = {
     padding: '20px', 
     fontWeight: 'bold',
-    fontSize: '20px'
+    fontSize: '20px',
+    fontFamily: 'Georgia'
   };
+  
